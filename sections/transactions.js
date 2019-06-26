48a98956765e06eeea830d566be1bf872b549922
Igor Savin <igor.savin@adidas.com>
2019-06-26 16:18:33 +0200

    Adjust documentation

--- sections/transactions.js
+++ sections/transactions.js
@@ -153,7 +153,7 @@ export default [
   },
   {
     type: "text",
-    content: "You can access promise that gets resolved after transaction is committed, or rejected if it gets rolled back, when using either way of creating transaction, from field `executionPromise`:"
+    content: "You can access promise that gets resolved after transaction is rolled back explicitly by user or committed, or rejected if it gets rolled back by DB itself, when using either way of creating transaction, from field `executionPromise`:"
   },
   {
     type: "code",
